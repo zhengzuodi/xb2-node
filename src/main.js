9c6e76848eef05981bdad66b3dcadfd619df87c9
hope <zhengzuodi@aliyun.com>
2020-12-18 23:32:11 +0800

    add express

--- src/main.js
+++ src/main.js
@@ -1,21 +1,44 @@
-const http = require('http');
-const server = http.createServer((request, response) => {
-  const data = {
+const { response } = require('express');
+const express = require('express');
+const app = express();
+const port = 3000;
+
+
+app.listen(port, () => {
+  console.log('🚀服务器已启动!');
+});
+
+
+app.get('/', (request, response) => {
+  response.send('你好');
+});
+
+
+const data = [
+  {
     id: 1,
     title: '关山月',
-    content: '明月出天山，苍茫云海间。'
+    content: '明月出天山，苍茫云海间'
+  },
+  {
+    id: 2,
+    title: '望月',
+    content: '会当凌绝顶，一览众山小'
+  },
+  {
+    id: 3,
+    title: '忆江南',
+    content: '日出江花红胜火，春来江水绿如蓝'
   }
-  const jsonData = JSON.stringify(data);
-  response.writeHead(200, {
-    'Content-Type': 'application/json; charset=utf-8'
-  });
+];
 
-  response.write(jsonData);
-  response.end();
-  
+app.get('/posts', (request, response) => {
+  response.send(data);
 });
 
-server.listen(80, () => {
-  console.log('🚀服务已启动！');
-});
+app.get('/posts/:postId', (request, response) => {
+  const { postId } = request.params;
 
+  const posts = data.filter( item => item.id = postId );
+  response.send(posts[0]);
+});
